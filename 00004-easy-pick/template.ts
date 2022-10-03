c94971e034fd5cfadf61135c0879c6309cce93ac
cuiyiming1998 <1269567664@qq.com>
2022-10-03 21:36:57 +0800

    ✨ feat: 构建刷题环境 实现04-MyPick

---
+++ 00004-easy-pick/template.ts
@@ -0,0 +1,14 @@
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P]
+}
+
+// js:
+function myPick(todo, keys) {
+	const obj = {}
+	keys.forEach(key => {
+		if (key in todo) {
+			obj[key] = obj[key]
+		}
+	})
+	return obj
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+
+### TypeScript类型体操训练
+
+题库地址 <a href="https://github.com/type-challenges/type-challenges" target="_blank">types-challenges</a>
