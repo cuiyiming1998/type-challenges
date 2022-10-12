7eb0f868a6ff2b594d9c45fafc51dd78b283e6f2
cuiyiming1998 <1269567664@qq.com>
2022-10-12 18:26:27 +0800

    ✨ feat: 实现easy-readonly

---
+++ 00007-easy-readonly/template.ts
@@ -0,0 +1,24 @@
+type MyReadonly<T> = {
+  readonly [P in keyof T]:  T[P]
+}
+
+// interface t {
+//   a: string,
+//   b: string
+// }
+// type r = keyof t  // 'a' | 'b'
+
+// js
+
+function readonly(obj) {
+  const result = {}
+  for (const key in obj) {
+    result['readonly' + key] = obj[key]
+  }
+  return result
+}
+
+// 1. 返回一个对象
+// 2. 遍历obj (ts -> interface)
+// 3. 加上readonly关键字
+// 4. 通过key获取interface里的值
